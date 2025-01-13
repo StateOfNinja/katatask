4673565917d1fe6df50a7de824f0c92dc349bec9
StateOfNinja <catbape@gmail.com>
2025-01-13 20:07:43 +0300

    сделал правки

--- src/script/script.js
+++ src/script/script.js
@@ -3,6 +3,76 @@ const sidebar = document.querySelector(".sidebar");
 const overlay = document.querySelector(".overlay");
 const continuationBtn = document.querySelectorAll(".continuation");
 const aboutText = document.querySelector(".about-us__text-content");
+const icons = document.querySelectorAll(".icon");
+const modalFeedback = document.querySelector(".modal-feedback");
+const modalCall = document.querySelector(".modal-call");
+const iconClose = document.querySelector(".icon--close");
+const modal = document.querySelector(".modal");
+
+// document.addEventListener("click", (e) => {
+//   if (
+//     burgerBtn.classList.contains("header__burger--active") &&
+//     !sidebar.contains(e.target) &&
+//     !burgerBtn.contains(e.target)
+//   ) {
+//     burgerBtn.classList.remove("header__burger--active");
+//     sidebar.classList.remove("sidebar--open");
+//     overlay.classList.remove("overlay--active");
+//   }
+// });
+
+burgerBtn.addEventListener("click", () => {
+  burgerBtn.classList.toggle("header__burger--active");
+  sidebar.classList.toggle("sidebar--open");
+  overlay.classList.toggle("overlay--active");
+});
+
+function openModal(modal) {
+  modal.classList.add("modal--active");
+  overlay.classList.add("overlay--active");
+  closeBurger();
+}
+
+function closeModal(modal) {
+  console.log("1");
+  modal.classList.remove("modal--active");
+  overlay.classList.remove("overlay--active");
+}
+
+function closeBurger() {
+  burgerBtn.classList.remove("header__burger--active");
+  sidebar.classList.remove("sidebar--open");
+}
+
+document.addEventListener("click", (e) => {
+  const target = e.target;
+
+  if (target.closest(".icon--message")) {
+    openModal(modalFeedback);
+  }
+  if (target.closest(".icon--phone")) {
+    openModal(modalCall);
+  }
+  if (
+    target.classList.contains("icon--close") ||
+    (overlay.classList.contains("overlay--active") &&
+      !target.closest(".modal") &&
+      !target.closest(".icon"))
+  ) {
+    console.log("2");
+    closeModal(modalFeedback);
+    closeModal(modalCall);
+  }
+  if (
+    burgerBtn.classList.contains("header__burger--active") &&
+    !sidebar.contains(e.target) &&
+    !burgerBtn.contains(e.target)
+  ) {
+    burgerBtn.classList.remove("header__burger--active");
+    sidebar.classList.remove("sidebar--open");
+    overlay.classList.remove("overlay--active");
+  }
+});
 
 if (window.innerWidth < 768) {
   const swiper = new Swiper(".swiper", {
